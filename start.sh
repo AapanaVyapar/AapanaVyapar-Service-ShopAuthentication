a34a75d438797517ec73548e040311c004f26f00
shitij <21.shitijagrawal@gmail.com>
2021-05-21 18:18:28 +0530

    docker compose and port of redis updated

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 export $(grep -v '^#' .env | xargs)
 
 echo $DBHost
+echo $DBPort
 
 #host="$DBHost"
 #host=`printf "%s:%s" "$host" "$DBPort"`
@@ -16,10 +17,10 @@ echo $DBHost
   
 #>&2 echo "Postgres is up - executing command"
 
-./wait-for-it.sh -h $DBHost -p 5432 -t 50 -- echo "PostGres Is Up"
+./wait-for-it.sh -h $DBHost -p $DBPort -t 50 -- echo "PostGres Is Up"
 
 sleep 30
 
-./wait-for-it.sh -h $DBHost -p 5432 -t 30 -- echo "PostGres Is Up"
+./wait-for-it.sh -h $DBHost -p $DBPort -t 30 -- echo "PostGres Is Up"
 
 ./main
